8788f0538a51c8370a700ad77d605c58a8278b6b
DeyanGeorgiev <gergov.deyan@gmail.com>
2020-01-04 20:31:24 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { data }  from  '../assets/beers.json';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'task2';
+  beers : any;
+  input: string;
+
+  order = "";
+  ascDsc= false;
+
+  ngOnInit(){
+  this.beers = data;
+  }
+
+  onChange(event){
+    this.order = event.target.title;
+    this.ascDsc = event.target.value;  
+  }
+  
 }
